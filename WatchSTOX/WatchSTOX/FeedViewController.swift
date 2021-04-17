7a2e97494402765fa3035d8df2369961daed32cd
Gazi Shahi <gazishahi@gmail.com>
2021-04-17 19:12:09 -0400

    login setup

---
+++ WatchSTOX/WatchSTOX/FeedViewController.swift
@@ -0,0 +1,41 @@
+//
+//  FeedViewController.swift
+//  WatchSTOX
+//
+//  Created by Gazi Shahi on 4/17/21.
+//
+
+import UIKit
+import Parse
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(identifier: "LoginViewController")
+        
+        let delegate = self.view.window?.windowScene?.delegate as! SceneDelegate
+        
+        delegate.window?.rootViewController = loginViewController
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
